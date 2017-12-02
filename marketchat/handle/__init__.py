b1e8ab669aeb12d4d5e831d64164185970790c89
Daniel <nieltansah@gmail.com>
2017-12-02 09:16:42 +0700

    Activate compare

--- marketchat/handle/__init__.py
+++ marketchat/handle/__init__.py
@@ -1,9 +1,10 @@
 from marketchat.util.router import Router
-from . import default, search, view
+from . import default, compare, search, view
 
 route = Router()
 
 route.handle(default.route)
+route.handle(compare.route)
 route.handle(search.route)
 route.handle(view.route)
 
