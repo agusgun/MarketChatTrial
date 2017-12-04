07e088c5a8f4dd295b3cbdda649f220b0ec52906
Daniel <nieltansah@gmail.com>
2017-12-04 16:38:42 +0700

    miss one

--- marketchat/handle/compare.py
+++ marketchat/handle/compare.py
@@ -17,7 +17,7 @@ def handle_compare(event, data):
 Price:
 (1) Rp {'{:,}'.format(a.price)}{f" ({round(a.promo * 100)}% off)" if a.promo is not None else ""}
 (2) Rp {'{:,}'.format(b.price)}{f" ({round(b.promo * 100)}% off)" if b.promo is not None else ""}
-Difference: Rp {abs(a.price - b.price)}
+Difference: Rp {'{:,}'.format(abs(a.price - b.price))}
 
 Store:
 (1) {a.store}
